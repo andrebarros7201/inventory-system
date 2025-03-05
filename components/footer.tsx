4eca56b3c338e68c68d168c6c8a72197d5dcc023
André Barros <andrebarros7201@gmail.com>
2025-03-05 20:00:45 +0000

    create basic footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full flex justify-center items-center border-2 border-red-500">
+      <h2>Developed By André Barros</h2>
+    </footer>
+  );
+};
+
+export default Footer;
