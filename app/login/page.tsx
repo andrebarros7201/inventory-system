2c4ef5f8ee698ffba1ebb0f7ad5b9c3738e32de2
André Barros <andrebarros7201@gmail.com>
2025-03-06 15:54:34 +0000

    create login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,47 @@
+"use client";
+import { FormEvent, useRef } from "react";
+import Input from "@/components/ui/input";
+
 const Login = () => {
-  return <main className="w-full flex flex-col items-center py-2"></main>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+  return (
+    <main className="w-full max-w-md flex flex-col items-center p-4 gap-4 bg-gray-700 rounded">
+      <h2 className={"font-bold text-2xl w-full text-start"}>Log In</h2>
+      <form
+        className={"w-full flex flex-col items-start gap-8"}
+        onSubmit={handleSubmit}
+      >
+        <Input
+          label={"username"}
+          placeholder={""}
+          id={"username"}
+          type={"text"}
+          required={true}
+          max={50}
+          min={3}
+          ref={usernameRef}
+        />
+        <Input
+          label={"password"}
+          placeholder={""}
+          id={"password"}
+          type={"text"}
+          required={true}
+          max={50}
+          min={3}
+          ref={passwordRef}
+        />
+        <button className={"bg-blue-500 py-2 px-4 font-bold rounded"}>
+          Log In
+        </button>
+      </form>
+    </main>
+  );
 };
 
 export default Login;
