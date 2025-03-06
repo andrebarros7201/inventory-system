8138a1ad44023ec7e8fb5475d541fff51f9288a5
André Barros <andrebarros7201@gmail.com>
2025-03-06 11:01:19 +0000

    change home div to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div
+    <main
       className={"flex-1 flex flex-col items-center justify-start gap-6 py-6"}
     >
       <header className={"w-full flex items-center justify-center"}>
         <h2 className={"italic font-bold text-2xl"}>Inventory System</h2>
       </header>
-    </div>
+    </main>
   );
 }
