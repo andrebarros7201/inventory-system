e44f770e8a623fe4fa7abc9d14e088773ddcf625
André Barros <andrebarros7201@gmail.com>
2025-03-06 11:08:51 +0000

    create login page file and update header links

---
+++ app/login/page.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <main className="w-full flex flex-col items-center py-2"></main>;
+};
+
+export default Login;
--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="w-full flex flex-row justify-center bg-blue-500 p-4">
       <div className="max-w-[1000px] w-full">
         <nav className={"flex items-center justify-start gap-4"}>
-          <h2 className={"font-bold text-2xl h-full"}>Inventory System</h2>
+          <Link href={"/"}>
+            <h2 className={"font-bold text-2xl h-full"}>Inventory System</h2>
+          </Link>
           <ul className={"h-full flex items-center justify-start"}>
-            <li>Login</li>
+            <Link href={"/login"}>Login</Link>
           </ul>
         </nav>
       </div>
