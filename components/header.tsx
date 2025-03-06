05292a5f5b96a7c86dc8ac7b1b259d249c2be216
André Barros <andrebarros7201@gmail.com>
2025-03-06 11:03:39 +0000

    update header style

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <header className="w-full flex flex-row justify-center bg-blue-500 p-4">
-      <div className="max-w-[1000px] w-full border-2 border-red-500">
-        <nav className={"flex items-center gap-4 "}>
-          <h2 className={"italic font-bold text-xl"}>Inventory System</h2>
-          <ul>
+      <div className="max-w-[1000px] w-full">
+        <nav className={"flex items-center justify-start gap-4"}>
+          <h2 className={"font-bold text-2xl h-full"}>Inventory System</h2>
+          <ul className={"h-full flex items-center justify-start"}>
             <li>Login</li>
           </ul>
         </nav>
