b5e5bba8e0150153d7c71dcbb8799dbef8b6d774
André Barros <andrebarros7201@gmail.com>
2025-03-08 11:36:45 +0000

    create user login api route

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/utils/prisma";
+import comparePassword from "@/utils/comparePassword";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { username, password } = await req.json();
+
+  // Get user from db
+  const user = await prisma.user.findUnique({ where: { username } });
+
+  if (!user) {
+    return NextResponse.json({ message: "User not found" }, { status: 404 });
+  }
+
+  const isPasswordCorrect = await comparePassword(password, user.password);
+  if (!isPasswordCorrect) {
+    return NextResponse.json(
+      { message: "Incorrect username/password combination" },
+      { status: 401 },
+    );
+  }
+
+  return NextResponse.json(
+    { message: "Logged in successfully" },
+    { status: 200 },
+  );
+}
