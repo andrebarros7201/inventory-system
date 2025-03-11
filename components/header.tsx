4072cde4a7ac1decbfb18cf3a32af086e9fa0367
André Barros <andrebarros7201@gmail.com>
2025-03-11 17:33:51 +0000

    add conditional render on header if user is logged in or not

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,19 @@
+"use client";
 import HeaderLink from "@/components/ui/header-link";
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { UserActions } from "@/redux/slicers/userSlice";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const { isLoggedIn } = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  function handleLogOut() {
+    dispatch(UserActions.logOut());
+    router.push("/");
+  }
   return (
     <header className="w-full flex flex-row justify-center bg-blue-500 p-4">
       <div className="max-w-[1000px] w-full">
@@ -10,10 +22,22 @@ const Header = () => {
             <h2 className={"font-bold text-2xl h-full"}>Inventory System</h2>
           </Link>
           <ul className={"h-full flex items-center justify-start"}>
-            <HeaderLink label={"Log In"} path={"/login"} />
-          </ul>
-          <ul className={"h-full flex items-center justify-start"}>
-            <HeaderLink label={"Sign up"} path={"/signup"} />
+            {!isLoggedIn ? (
+              <>
+                <li>
+                  <HeaderLink label={"Log In"} path={"/login"} />
+                </li>
+                <li>
+                  <HeaderLink label={"Sign up"} path={"/signup"} />
+                </li>
+              </>
+            ) : (
+              <li>
+                <button className={"cursor-pointer"} onClick={handleLogOut}>
+                  Log Out
+                </button>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
