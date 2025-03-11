b934ff896ecf911e11f86cd002963f2f8b304ce2
André Barros <andrebarros7201@gmail.com>
2025-03-11 21:39:17 +0000

    send user username and id on log in

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -15,7 +15,10 @@ export async function POST(req: Request) {
   const isPasswordCorrect = await comparePassword(password, user.password);
   if (!isPasswordCorrect) {
     return NextResponse.json(
-      { message: "Incorrect username/password combination" },
+      {
+        message: "Incorrect username/password combination",
+        user: { username: user.username, userID: user.userID },
+      },
       { status: 401 },
     );
   }
