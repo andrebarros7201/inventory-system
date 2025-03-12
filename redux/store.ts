8eb956dccfd4ad896c806337f85b20977c45a7a1
André Barros <andrebarros7201@gmail.com>
2025-03-12 17:48:13 +0000

    add storeSlice and productSlice to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "@/redux/slicers/userSlice";
 import notificationSlice from "@/redux/slicers/notificationSlice";
+import storeSlice from "@/redux/slicers/storeSlice";
+import productSlice from "@/redux/slicers/productSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     notification: notificationSlice,
+    store: storeSlice,
+    product: productSlice,
   },
 });
 
