1bde19943c5528a72da4697d2211137106df3962
André Barros <andrebarros7201@gmail.com>
2025-03-12 19:59:46 +0000

    get users stores

--- app/api/store/route.ts
+++ app/api/store/route.ts
@@ -20,3 +20,18 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    const { userID } = await req.json();
+
+    const stores = await prisma.store.findMany({ where: { userID } });
+
+    return NextResponse.json({ stores }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Server error: ${error}` },
+      { status: 500 },
+    );
+  }
+}
