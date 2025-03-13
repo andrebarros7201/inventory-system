e66c0d9c78e3275b03411a46c4d50c00d4d4cad1
André Barros <andrebarros7201@gmail.com>
2025-03-13 18:25:51 +0000

    edit store modal

--- components/store/displayStoresItem.tsx
+++ components/store/displayStoresItem.tsx
@@ -2,7 +2,9 @@ import Button from "@/components/ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { NotificationActions } from "@/redux/slicers/notificationSlice";
 import { fetchStores } from "@/redux/slicers/storeSlice";
-import { RootState } from "@/redux/store";
+import { AppDispatch, RootState } from "@/redux/store";
+import { FormEvent, useRef, useState } from "react";
+import Input from "@/components/ui/input";
 
 type Props = {
   store: { storeID: string; name: string; userID: string };
@@ -10,8 +12,10 @@ type Props = {
 };
 
 const DisplayStoreItem = ({ store, index }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const newStoreNameRef = useRef<HTMLInputElement>(null);
   const { user } = useSelector((state: RootState) => state.user);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   async function handleDelete(id: string) {
     try {
@@ -32,10 +36,49 @@ const DisplayStoreItem = ({ store, index }: Props) => {
       );
       dispatch(fetchStores(user.userID));
     } catch (error) {
+      console.error(error);
       dispatch(
         NotificationActions.createNotification({
           type: "error",
           message: "Something went wrong",
+        }),
+      );
+    }
+  }
+
+  async function handleUpdate(e: FormEvent) {
+    e.preventDefault();
+    if (!user) return;
+    try {
+      const response = await fetch(`/api/store`, {
+        method: "PATCH",
+        headers: {
+          contentType: "application/json",
+        },
+        body: JSON.stringify({
+          name: newStoreNameRef.current!.value,
+          storeID: store.storeID,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Could not update store");
+      }
+
+      dispatch(fetchStores(user.userID));
+
+      dispatch(
+        NotificationActions.createNotification({
+          type: "success",
+          message: "Store updated",
+        }),
+      );
+    } catch (error) {
+      console.error(error);
+      dispatch(
+        NotificationActions.createNotification({
+          type: "error",
+          message: "Failed to update store",
         }),
       );
     }
@@ -53,7 +96,13 @@ const DisplayStoreItem = ({ store, index }: Props) => {
       </div>
       <div className={"flex gap-4 items-center"}>
         <Button label={"Open"} color={"blue"} type={"button"} bold />
-        <Button label={"Edit"} color={"amber"} type={"button"} bold />
+        <Button
+          label={"Edit"}
+          color={"amber"}
+          type={"button"}
+          onClick={() => setIsEditing(!isEditing)}
+          bold
+        />
         <Button
           label={"Delete"}
           color={"red"}
@@ -62,6 +111,50 @@ const DisplayStoreItem = ({ store, index }: Props) => {
           bold
         />
       </div>
+      {isEditing && (
+        <div
+          onClick={() => setIsEditing(false)}
+          className="h-dvw w-dvw fixed top-0 left-0 flex justify-center items-center bg-gray-700/70"
+        >
+          <div
+            onClick={(event) => event.stopPropagation()}
+            className="bg-gray-700 p-8 rounded shadow-lg top-10 fixed flex flex-col items-start justify-start gap-4"
+          >
+            <div className="flex items-center justify-between w-full">
+              <h2 className={"font-bold text-2xl w-full text-start"}>
+                Update Store
+              </h2>
+              <button
+                onClick={() => setIsEditing(!isEditing)}
+                className={"text-red-600 font-extrabold text-lg cursor-pointer"}
+              >
+                X
+              </button>
+            </div>
+            <form
+              className={"flex flex-col gap-8 items-start"}
+              onSubmit={handleUpdate}
+            >
+              <Input
+                label={"name"}
+                id={"name"}
+                type={"text"}
+                required={true}
+                min={3}
+                ref={newStoreNameRef}
+              />
+
+              <button
+                className={
+                  "bg-blue-500 hover:bg-blue-600 rounded p-4 cursor-pointer transition-all duration-300"
+                }
+              >
+                Add Store
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
