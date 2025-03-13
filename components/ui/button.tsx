1a9690a550e2b1808e53e1eed5542dff7419ab78
André Barros <andrebarros7201@gmail.com>
2025-03-13 13:19:57 +0000

    add default values for bold and type props and define possible values for type prop

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,12 +2,20 @@ type Props = {
   label: string;
   color: string;
   bold?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
+  type: "submit" | "reset" | "button" | undefined;
 };
 
-const Button = ({ label, color, bold, onClick }: Props) => {
+const Button = ({
+  label,
+  color,
+  bold = false,
+  onClick,
+  type = "button",
+}: Props) => {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={`bg-${color}-500 px-4 py-2 ${bold ? "font-bold" : ""} hover:bg-${color}-700 cursor-pointer transition duration-300 rounded`}
     >
