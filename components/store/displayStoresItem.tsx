428c865e3dbf517dee0b65c7b103e60988b6b898
André Barros <andrebarros7201@gmail.com>
2025-03-13 14:05:42 +0000

    use custom button on store items

--- components/store/displayStoresItem.tsx
+++ components/store/displayStoresItem.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/ui/button";
+
 type Props = {
   store: { storeID: string; name: string; userID: string };
   index: number;
@@ -15,27 +17,9 @@ const DisplayStoresItem = ({ store, index }: Props) => {
         <p>{store.name}</p>
       </div>
       <div className={"flex gap-4 items-center"}>
-        <button
-          className={
-            "px-4 py-2 bg-blue-500 rounded hover:bg-blue-600 transition-all duration-300 cursor-pointer"
-          }
-        >
-          Open
-        </button>
-        <button
-          className={
-            "px-4 py-2 bg-amber-600 rounded  hover:bg-amber-700 transition-all duration-300 cursor-pointer"
-          }
-        >
-          Edit
-        </button>
-        <button
-          className={
-            "px-4 py-2 bg-red-600 rounded  hover:bg-red-700 transition-all duration-300 cursor-pointer"
-          }
-        >
-          Delete
-        </button>
+        <Button label={"Open"} color={"blue"} type={"button"} />
+        <Button label={"Edit"} color={"amber"} type={"button"} />
+        <Button label={"Delete"} color={"green"} type={"button"} />
       </div>
     </div>
   );
