5a0b2d5e0249e47e61c617b1ddb3d711bdd0fb78
André Barros <andrebarros7201@gmail.com>
2025-03-13 15:39:07 +0000

    add fetchStores thunk to store slice

--- redux/slicers/storeSlice.ts
+++ redux/slicers/storeSlice.ts
@@ -1,8 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import StoreSlice from "@/types/StoreSlice";
 
-const initialState = {
+const initialState: StoreSlice = {
   stores: [],
+  loading: false,
+  error: null,
 };
+
+export const fetchStores = createAsyncThunk(
+  "store/fetchStores",
+  async (userID: string) => {
+    const response = await fetch(`api/store?userID=${userID}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch stores.");
+    }
+    return response.json();
+  },
+);
 
 const storeSlice = createSlice({
   name: "store",
@@ -14,6 +28,20 @@ const storeSlice = createSlice({
     clearStores: (state) => {
       state.stores = [];
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchStores.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchStores.fulfilled, (state, action) => {
+        state.stores = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchStores.rejected, (state) => {
+        state.loading = false;
+        state.error = "Failed to fetch stores.";
+      });
   },
 });
 
