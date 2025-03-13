d46d0290ebf87a6abaca79356e1769d978154fb7
André Barros <andrebarros7201@gmail.com>
2025-03-13 14:09:41 +0000

    use clsx on custom button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,8 @@
+import clsx from "clsx";
+
 type Props = {
   label: string;
-  color: string;
+  color: "red" | "green" | "blue" | "amber";
   bold?: boolean;
   onClick?: () => void;
   type?: "submit" | "reset" | "button" | undefined;
@@ -17,7 +19,16 @@ const Button = ({
     <button
       type={type}
       onClick={onClick}
-      className={`bg-${color}-500 px-4 py-2 ${bold ? "font-bold" : ""} hover:bg-${color}-700 cursor-pointer transition duration-300 rounded`}
+      className={clsx(
+        "px-4 py-2 rounded transition duration-300 cursor-pointer",
+        {
+          "font-bold": bold,
+          "bg-red-500 hover:bg-red-600": color === "red",
+          "bg-blue-500 hover:bg-blue-600": color === "blue",
+          "bg-green-500 hover:bg-green-600": color === "green",
+          "bg-amber-500 hover:bg-amber-600": color === "amber",
+        },
+      )}
     >
       {label}
     </button>
