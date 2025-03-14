7bad5f609f8f7adbcf8a65972df8c7b66fd79dff
André Barros <andrebarros7201@gmail.com>
2025-03-14 20:38:45 +0000

    create productSlice and product interfaces

---
+++ types/Product.ts
@@ -0,0 +1,5 @@
+export default interface Product {
+  productID: string;
+  name: string;
+  quantity: number;
+}
---
+++ types/ProductSlice.ts
@@ -0,0 +1,6 @@
+import Product from "./Product";
+
+export default interface ProductSlice {
+  store: string;
+  products: Product[];
+}
