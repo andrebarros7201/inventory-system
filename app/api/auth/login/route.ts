617d34ac19b13fcb35188e18f9f1ca1b00590320
André Barros <andrebarros7201@gmail.com>
2025-03-14 18:32:11 +0000

    improve login get api method

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,29 +3,46 @@ import comparePassword from "@/utils/comparePassword";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { username, password } = await req.json();
+  try {
+    const { username, password } = await req.json();
+    // Get user from db
+    const user = await prisma.user.findUnique({ where: { username } });
 
-  // Get user from db
-  const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
 
-  if (!user) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
-  }
+    // Compare passwords
+    const isPasswordCorrect = await comparePassword(password, user.password);
 
-  const isPasswordCorrect = await comparePassword(password, user.password);
-  if (!isPasswordCorrect) {
+    if (!isPasswordCorrect) {
+      return NextResponse.json(
+        {
+          notification: {
+            type: "error",
+            message: "Incorrect username/password combination",
+          },
+        },
+        { status: 401 },
+      );
+    }
+
+    // Success
     return NextResponse.json(
-      { message: "Incorrect username/password combination" },
-      { status: 401 },
+      {
+        user: { username: user.username, userID: user.userID },
+        notification: {
+          type: "success",
+          message: "Logged in successfully",
+        },
+      },
+
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { notification: { type: "error", message: "Server error2" } },
+      { status: 500 },
     );
   }
-
-  return NextResponse.json(
-    {
-      message: `Logged in successfully`,
-      user: { username: user.username, userID: user.userID },
-    },
-
-    { status: 200 },
-  );
 }
