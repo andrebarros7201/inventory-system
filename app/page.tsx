ed9b9c15b38a45d8fb828cc659cb83ba03672796
André Barros <andrebarros7201@gmail.com>
2025-03-16 11:52:45 +0000

    add homeCard to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import { HomeCard } from "@/components/ui/homeCard";
+
 export default function Home() {
   return (
     <main
-      className={"flex-1 flex flex-col items-center justify-start gap-6 py-6"}
+      className={
+        "flex-1 w-full border-2 border-red-500 flex flex-col items-center justify-start gap-6 py-6"
+      }
     >
       <header className={"w-full flex items-center justify-center"}>
-        <h2 className={"italic font-bold text-2xl"}>Inventory System</h2>
+        <h2 className={"italic font-bold text-3xl"}>Inventory System</h2>
       </header>
+      <div className={"flex flex-col sm:grid grid-cols-2 gap-4"}>
+        <HomeCard
+          title={"Manage Stores"}
+          description={"Create, Edit and Manage your stores"}
+          image={"/store.svg"}
+          imageAlt={"image of a store"}
+          buttonText={"Start Now"}
+          buttonLink={"/register"}
+        />
+        <HomeCard
+          title={"Manage Stores Item"}
+          description={"Create, Edit and Manage your items"}
+          image={"/fruit-pear.svg"}
+          imageAlt={"image of a pear"}
+          buttonText={"Start Now"}
+          buttonLink={"/register"}
+        />
+      </div>
     </main>
   );
 }
